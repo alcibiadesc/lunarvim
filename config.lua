f4516d2e3b8e989c3f39c1caec2f648e2470f77b
alcibiadesc <acabdia@gmail.com>
2023-04-11 23:34:58 +0100

    added

---
+++ config.lua
@@ -0,0 +1,186 @@
+-- general
+lvim.log.level = "warn"
+lvim.format_on_save.enabled = true
+lvim.colorscheme = "edge"
+vim.opt.relativenumber = true
+lvim.keys.normal_mode["<Space><Space>"] = ":NvimTreeToggle<CR>"
+
+
+
+
+-- leader s = :split
+vim.api.nvim_set_keymap('n', '<leader>s', ':split<CR>', { noremap = true, silent = true })
+
+-- leader v = :vsplit
+vim.api.nvim_set_keymap('n', '<leader>v', ':vsplit<CR>', { noremap = true, silent = true })
+
+-- Fugitive leader G
+
+vim.api.nvim_set_keymap('n', '<leader>g', ':G<CR>', { noremap = true, silent = true })
+
+---- MAP
+
+vim.api.nvim_set_keymap('n', '>', ':bprev<CR>', { noremap = true, silent = true })
+
+-- Cambiar a la pestaña anterior usando la tecla '<'
+vim.api.nvim_set_keymap('n', '<', ':bnext<CR>', { noremap = true, silent = true })
+
+
+
+
+
+
+
+
+-- TODO: User Config for predefined plugins
+-- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
+lvim.builtin.alpha.active = true
+lvim.builtin.alpha.mode = "dashboard"
+lvim.builtin.terminal.active = true
+lvim.builtin.nvimtree.setup.view.side = "left"
+lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
+
+-- if you don't want all the parsers change this to a table of the ones you want
+lvim.builtin.treesitter.ensure_installed = {
+  "bash",
+  "c",
+  "javascript",
+  "json",
+  "lua",
+  "python",
+  "typescript",
+  "tsx",
+  "css",
+  "rust",
+  "java",
+  "yaml",
+}
+
+lvim.builtin.treesitter.ignore_install = { "haskell" }
+lvim.builtin.treesitter.highlight.enable = true
+
+-- generic LSP settings
+
+-- -- make sure server will always be installed even if the server is in skipped_servers list
+lvim.lsp.installer.setup.ensure_installed = {
+  "svelte",
+}
+-- -- change UI setting of `LspInstallInfo`
+-- -- see <https://github.com/williamboman/nvim-lsp-installer#default-configuration>
+-- lvim.lsp.installer.setup.ui.check_outdated_servers_on_open = false
+-- lvim.lsp.installer.setup.ui.border = "rounded"
+-- lvim.lsp.installer.setup.ui.keymaps = {
+--     uninstall_server = "d",
+--     toggle_server_expand = "o",
+-- }
+
+-- ---@usage disable automatic installation of servers
+-- lvim.lsp.installer.setup.automatic_installation = false
+
+-- ---configure a server manually. !!Requires `:LvimCacheReset` to take effect!!
+-- ---see the full default list `:lua print(vim.inspect(lvim.lsp.automatic_configuration.skipped_servers))`
+-- vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "pyright" })
+local opts = {} -- check the lspconfig documentation for a list of all possible options
+require("lvim.lsp.manager").setup("tailwindcss", opts)
+require("lvim.lsp.manager").setup("eslint", opts)
+-- require("lvim.lsp.manager").setup("pyright", opts)
+
+-- ---remove a server from the skipped list, e.g. eslint, or emmet_ls. !!Requires `:LvimCacheReset` to take effect!!
+-- ---`:LvimInfo` lists which server(s) are skipped for the current filetype
+-- lvim.lsp.automatic_configuration.skipped_servers = vim.tbl_filter(function(server)
+--   return server ~= "emmet_ls"
+-- end, lvim.lsp.automatic_configuration.skipped_servers)
+
+-- -- you can set a custom on_attach function that will be used for all the language servers
+-- -- See <https://github.com/neovim/nvim-lspconfig#keybindings-and-completion>
+-- lvim.lsp.on_attach_callback = function(client, bufnr)
+--   local function buf_set_option(...)
+--     vim.api.nvim_buf_set_option(bufnr, ...)
+--   end
+--   --Enable completion triggered by <c-x><c-o>
+--   buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
+-- end
+
+-- -- set a formatter, this will override the language server formatting capabilities (if it exists)
+local formatters = require "lvim.lsp.null-ls.formatters"
+formatters.setup {
+  {
+    command = "prettier",
+    filetypes = { "typescript", "typescriptreact", "javascript", "svelte", "css" },
+  },
+}
+
+-- set additional linters
+local linters = require "lvim.lsp.null-ls.linters"
+linters.setup {
+  { command = "eslint", filetypes = { "typescript", "typescriptreact", "javascript", "svelte", "css" } }
+}
+--
+
+-- CUSTOM
+--
+lvim.plugins = {
+
+  {
+    "tpope/vim-fugitive",
+    cmd = {
+      "G",
+      "Git",
+      "Gdiffsplit",
+      "Gread",
+      "Gwrite",
+      "Ggrep",
+      "GMove",
+      "GDelete",
+      "GBrowse",
+      "GRemove",
+      "GRename",
+      "Glgrep",
+      "Gedit"
+    },
+    ft = { "fugitive" }
+  },
+  { "scrooloose/nerdtree" },
+  {
+    "windwp/nvim-ts-autotag",
+    config = function()
+      require("nvim-ts-autotag").setup()
+    end,
+  },
+  { "ellisonleao/gruvbox.nvim" },
+  { "sainnhe/edge" },
+  { "Mofiqul/dracula.nvim" },
+  { "metakirby5/codi.vim" },
+  { "kqito/vim-easy-replace" },
+  {
+    "phaazon/hop.nvim",
+    event = "BufRead",
+    config = function()
+      require("hop").setup()
+      vim.api.nvim_set_keymap("", "f", ":HopChar2<cr>", { silent = true })
+      vim.api.nvim_set_keymap("", "F", ":HopWord<cr>", { silent = true })
+
+    end,
+  },
+
+  {
+    "f-person/auto-dark-mode.nvim",
+    config = function()
+      local auto_dark_mode = require('auto-dark-mode')
+
+      auto_dark_mode.setup({
+        update_interval = 1000,
+        set_dark_mode = function()
+
+          vim.api.nvim_set_option('background', 'dark')
+        end,
+        set_light_mode = function()
+          vim.api.nvim_set_option('background', 'light')
+        end,
+      })
+
+      auto_dark_mode.init()
+    end,
+  },
+
+}
