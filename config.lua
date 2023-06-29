2d83e79d02aa7e5bbc2beb0f211ffcb0a4dc377f
alcibiadesc <acabdia@gmail.com>
2023-06-29 21:54:26 +0100

    change true

--- config.lua
+++ config.lua
@@ -36,7 +36,7 @@ local function setup_plugins()
   lvim.builtin.alpha.mode = "dashboard"
   lvim.builtin.terminal.active = true
   lvim.builtin.nvimtree.setup.view.side = "left"
-  lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
+  lvim.builtin.nvimtree.setup.renderer.icons.show.git = true 
 end
 
 -- Setup treesitter
